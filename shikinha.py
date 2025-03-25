59fa1b3cbeef8f3fc2535de246679d8119c092a6
madmatt <mpqfreitas@gmail.com>
2025-03-25 13:36:30 -0300

    Update shikinha.py

--- shikinha.py
+++ shikinha.py
@@ -152,9 +152,7 @@ async def play_next(ctx):
             print(e)
             await vc.disconnect()
     else:
-        await asyncio.sleep(60)
-        if not ctx.voice_client.is_playing() and ctx.voice_client:
-            await ctx.voice_client.disconnect()
+        await ctx.voice_client.disconnect()
 
 @client.command(aliases=["p", "tocar"])
 async def play(ctx, *, url):
